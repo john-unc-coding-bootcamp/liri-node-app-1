66397662d747e342365c4bf238a6c3db0452d173
Caroline Bowes <carvbowes@gmail.com>
2017-07-20 23:37:35 -0400

    twitter command

--- liri.js
+++ liri.js
@@ -1,33 +1,36 @@
+//Twitter config
 var keys = require("./keys.js");
-
 var Twitter = require('twitter');
-var client = new Twitter({
-  consumer_key: keys.twitterKeys.consumer_key,
-  consumer_secret: keys.twitterKeys.consumer_secret,
-  access_token_key: keys.twitterKeys.access_token_key,
-  access_token_secret: keys.twitterKeys.access_token_secret
-});
-console.log(client);
+var client = new Twitter(keys.twitterKeys);
+var params = {
+	screen_name: "carvbowes",
+	count: 2
+}
+
+//Spotify config
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
   id: '06de408f823c46b7921e97879bff734b',
   secret: '7419baf2c1dc46f9802f8989a360a1ca'
 });
 
+//Request
 var request = require("request");
 
 var command = process.argv[2];
 var search = process.argv[3];
-var params = {
-	screen_name: "carvbowes",
-	count: 20
-}
 
 function showTweets() {
-	client.get("search/tweets", params, function(error, response, body) {
+	client.get("statuses/user_timeline", params, function(error, response, body) {
 
-			if (!error && response.statusCode === 200) {
-				console.log(body);
+			if (!error) {
+				console.log("****************");
+				for (var i=0; i<response.length; i++) {
+					console.log("-----" + response[i].created_at + "-----");
+					console.log(response[i].text)
+				}
+				console.log("****************");
+
 			} else {
 				console.log(error);
 			}
@@ -54,6 +57,7 @@ function movieData(query) {
 }
 
 
+
 switch(command) {
 	case "my-tweets": {
 		showTweets();
@@ -73,6 +77,7 @@ switch(command) {
 	}
 
 	case "do-what-it-says": {
+
 		break;
 	}
 
